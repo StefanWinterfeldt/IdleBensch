68d673c728f7db733217328021b964295f98344a
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-15 18:16:33 +0200

    changed code style

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode ((640, 480))
 screen.fill ((0, 0, 0))
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    for event in pygame.event.get ():
+        if event.type == pygame.QUIT: sys.exit ()
 
     pygame.display.flip ()
