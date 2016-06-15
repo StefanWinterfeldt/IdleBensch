3284cf5746a153922b90d1ad71394812790ec6bd
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-15 19:01:28 +0200

    added game state and frame rate limitation

--- engine/initialization.py
+++ engine/initialization.py
@@ -1,9 +1,11 @@
 import constants.display as DC
+import globals.gameState as gameState
 import pygame
 
 
 def initialize ():
     pygame.init ()
+    gameState.clock = pygame.time.Clock ()
     screen = pygame.display.set_mode (DC.RESOLUTION, pygame.FULLSCREEN | pygame.HWSURFACE)
     screen.fill ((0, 0, 0))
     return screen
--- engine/mainLoop.py
+++ engine/mainLoop.py
@@ -1,10 +1,14 @@
+import constants.display as DC
 import engine.keyHandler as keyHandler
+import globals.gameState as gameState
 import pygame
 import sys
 
 
 def loop ():
     while True:
+        gameState.clock.tick (DC.FRAMERATE)
+
         for event in pygame.event.get ():
             if event.type == pygame.QUIT: sys.exit ()
             if event.type == pygame.KEYDOWN: keyHandler.handleKeyEvent (event)
