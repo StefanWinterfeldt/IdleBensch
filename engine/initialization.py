67ce77b09bd2f3d4055ebf402d9ccd64acb3cc21
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-15 19:04:22 +0200

    added global view container and color constants

--- engine/initialization.py
+++ engine/initialization.py
@@ -1,11 +1,12 @@
+import constants.color as color
 import constants.display as DC
 import globals.gameState as gameState
+import globals.view as view
 import pygame
 
 
 def initialize ():
     pygame.init ()
     gameState.clock = pygame.time.Clock ()
-    screen = pygame.display.set_mode (DC.RESOLUTION, pygame.FULLSCREEN | pygame.HWSURFACE)
-    screen.fill ((0, 0, 0))
-    return screen
+    view.screen = pygame.display.set_mode (DC.RESOLUTION, pygame.FULLSCREEN | pygame.HWSURFACE)
+    view.screen.fill (color.BLACK)
