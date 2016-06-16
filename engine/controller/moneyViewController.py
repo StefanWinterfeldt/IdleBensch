ae33a4175a083cf23ce083889f145748c5bd1d0d
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-16 18:03:53 +0200

    added episode heading

--- engine/controller/moneyViewController.py
+++ engine/controller/moneyViewController.py
@@ -1,10 +1,18 @@
 import constants.color as color
 import constants.display as DC
+import engine.util.draw as draw
+import globals.gameState as GS
 import globals.view as view
 import pygame
 
+def drawHeadings ():
+    episodeHeading = GS.font.render ('Folgen:', True, color.WHITE)
+    draw.drawXCentered (episodeHeading, view.moneyView, 2)
+
 def initialize ():
-    view.moneyView = view.mainView.subsurface ((0, DC.CLICKVIEWSIZE[1]-1, DC.MONEYVIEWSIZE [0], DC.MONEYVIEWSIZE [1]))
+    view.moneyView = view.mainView.subsurface ((0, DC.CLICKVIEWSIZE [1] - 1, DC.MONEYVIEWSIZE [0], DC.MONEYVIEWSIZE [1]))
+    pygame.draw.rect (view.moneyView, color.DARKGREEN, (0, 0, DC.MONEYVIEWSIZE [0] - 1, DC.MONEYVIEWSIZE [1] - 1), 2)
+    drawHeadings ()
 
 def update ():
-    pygame.draw.rect (view.moneyView, color.DARKGREEN, (0, 0, DC.MONEYVIEWSIZE [0] - 1, DC.MONEYVIEWSIZE [1] - 1), 2)
+    pass
