8ecca0a0568d020c7e9b997596b0846856d27fb2
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-16 18:34:05 +0200

    added episode completion circle

--- engine/controller/clickViewController.py
+++ engine/controller/clickViewController.py
@@ -3,22 +3,39 @@ import constants.display as DC
 import engine.service.idleGameNameGenerator as IGNG
 import engine.util.color as colorUtil
 import engine.util.draw as draw
+import globals.gameState as GS
 import globals.view as view
+import math
 import pygame
 import random
 
+
+completionCircleFullRadius = None
+completionCirclePos = None
+
+def drawCompletionCircle ():
+    pygame.draw.circle (view.clickView, color.BLACK, completionCirclePos, completionCircleFullRadius)
+    currentRadius = int ((completionCircleFullRadius / 100.0) * GS.episodeCompletionPercentage)
+    pygame.draw.circle (view.clickView, color.DARKGREEN, completionCirclePos, currentRadius)
+
 def handleClick (position):
     randomize ()
+    GS.episodeCompletionPercentage += 5
+    if GS.episodeCompletionPercentage > 100: GS.episodeCompletionPercentage = 0
 
 def initialize ():
+    global completionCirclePos
+    global completionCircleFullRadius
     view.clickView = view.mainView.subsurface ((0, 0, DC.CLICKVIEWSIZE [0], DC.CLICKVIEWSIZE [1]))
-    randomize()
+    completionCirclePos = (DC.CLICKVIEWSIZE [0] / 2, DC.CLICKVIEWSIZE [1] / 5)
+    completionCircleFullRadius = DC.CLICKVIEWSIZE [0] / 8
+    randomize ()
 
 def randomize ():
     backgroundColor = random.choice (color.CLICKVIEWCOLORS)
     view.clickView.fill (backgroundColor)
-    draw.drawCentered (IGNG.generateIdleGameName (colorUtil.invertColor(backgroundColor), backgroundColor), view.clickView)
+    draw.drawCentered (IGNG.generateIdleGameName (colorUtil.invertColor (backgroundColor), backgroundColor), view.clickView)
     pygame.draw.rect (view.clickView, color.DARKGREEN, (0, 0, DC.CLICKVIEWSIZE [0] - 1, DC.CLICKVIEWSIZE [1] - 1), 2)
 
 def update ():
-   pass
+    drawCompletionCircle ()
--- globals/gameState.py
+++ globals/gameState.py
@@ -1,3 +1,4 @@
 clock = None
+episodeCompletionPercentage = 0
 font = None
 fontName = None
