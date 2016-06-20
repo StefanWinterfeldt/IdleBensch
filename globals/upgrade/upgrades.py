101a502a19408f66638576028ae89cb9a7eb6c1b
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-06-20 19:31:58 +0200

    changed game logic to realistic levels and removed some test upgrades

--- globals/upgrade/upgrades.py
+++ globals/upgrade/upgrades.py
@@ -4,32 +4,10 @@ import os
 def upgrade1Activation ():
     print 'test1'
 
-def upgrade2Activation ():
-    print 'test2'
-
-def upgrade3Activation ():
-    print 'test3'
-
 testUpgrade1 = Upgrade (
     os.path.join ('resources', 'ingame', 'upgrade', 'defaultUpgrade.png'),
     'Upgrade1',
-    lambda : True,
-    lambda : True,
+    lambda: True,
+    lambda: True,
     upgrade1Activation
-)
-
-testUpgrade2 = Upgrade (
-    os.path.join ('resources', 'ingame', 'upgrade', 'defaultUpgrade.png'),
-    'Upgrade2',
-    lambda : True,
-    lambda : testUpgrade1.active,
-    upgrade2Activation
-)
-
-testUpgrade3 = Upgrade (
-    os.path.join ('resources', 'ingame', 'upgrade', 'defaultUpgrade.png'),
-    'Upgrade3',
-    lambda : True,
-    lambda : testUpgrade2.active,
-    upgrade3Activation
 )
