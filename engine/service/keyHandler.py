99769be5894cbfc25669af7f830ac52cb6b8fc2e
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-07-02 20:04:49 +0200

    added dev mode

--- engine/service/keyHandler.py
+++ engine/service/keyHandler.py
@@ -1,6 +1,12 @@
+import engine.controller.upgradeViewController as upgradeViewController
+import engine.util.upgrade as upgradeUtil
+import globals.gameLogic as GL
 import pygame
 import sys
 
 def handleKeyEvent (event):
     if event.key == pygame.K_ESCAPE:
         sys.exit ()
+    elif event.key == pygame.K_v and GL.DEV_MODE:
+        upgradeUtil.makeAllUpgradesVisible ()
+        upgradeViewController.drawCategories ()
--- engine/util/upgrade.py
+++ engine/util/upgrade.py
@@ -1,5 +1,14 @@
 import globals.upgrade.categories
 
+def makeAllUpgradesVisible ():
+    for header in getAllUpgradeHeaders ():
+        header.visible = True
+    for upgrade in getAllUpgrades ():
+        upgrade.visible = True
+
+def getAllUpgradeHeaders ():
+    return [category.header for category in globals.upgrade.categories.categories]
+
 def getAllUpgrades ():
     allUpgrades = []
     for category in globals.upgrade.categories.categories:
@@ -7,8 +16,8 @@ def getAllUpgrades ():
     return allUpgrades
 
 def getUpgradeById (upgradeId):
-    matchingUpgrades = [upgrade for upgrade in getAllUpgrades() if upgrade.id == upgradeId]
-    if len(matchingUpgrades) == 1:
-        return matchingUpgrades[0]
+    matchingUpgrades = [upgrade for upgrade in getAllUpgrades () if upgrade.id == upgradeId]
+    if len (matchingUpgrades) == 1:
+        return matchingUpgrades [0]
     else:
         return None
