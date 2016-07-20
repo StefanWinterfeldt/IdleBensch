d8f699904141e19390a54152aa7b1c319e8866fd
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2016-07-20 18:58:46 +0200

    added copyright notices and GPL license

--- engine/controller/passiveIncome.py
+++ engine/controller/passiveIncome.py
@@ -1,3 +1,19 @@
+#Copyright 2016 Stefan Winterfeldt
+#This file is part of Idle Bensch.
+#
+#Idle Bensch is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Idle Bensch is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Idle Bensch.  If not, see <http://www.gnu.org/licenses/>.
+
 import constants.display as CD
 import engine.controller.clickViewController as clickViewController
 import engine.service.modifiedGameLogic as MGL
@@ -9,18 +25,6 @@ import random
 ticksSinceLastProcess = 0
 currentAutoClicks = 0
 currentSubscribers = 0
-
-# def processMerchPurchases ():
-#     purchases = 0
-#     purchaseAmount = 0
-#     purchaseChance = MGL.getPurchaseChancePerSubscriberPerSecond ()
-#     for i in range (int (GS.subscriber)):
-#         if random.random () < purchaseChance:
-#             purchases += 1
-#     for i in range (purchases):
-#         purchaseCents = random.randint (math.floor (MGL.getMinPurchase () * 100), math.floor (MGL.getMaxPurchase () * 100))
-#         purchaseAmount += (purchaseCents * 0.01)
-#     GS.money += purchaseAmount
 
 def processMerchPurchases ():
     purchaseChance = MGL.getPurchaseChancePerSubscriberPerSecond ()
--- engine/initialization.py
+++ engine/initialization.py
@@ -1,3 +1,19 @@
+#Copyright 2016 Stefan Winterfeldt
+#This file is part of Idle Bensch.
+#
+#Idle Bensch is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Idle Bensch is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Idle Bensch.  If not, see <http://www.gnu.org/licenses/>.
+
 import constants.display as DC
 import engine.controller.gameViewController as gameViewController
 import engine.controller.menuViewController as menuViewController
